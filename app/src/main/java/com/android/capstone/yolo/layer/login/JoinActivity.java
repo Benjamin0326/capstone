2755fef45434fceda57f1012343b4f8fe17b003e
JangKyungJoo <rudwn826@gmail.com>
2017-05-12 02:45:11 +0900

    update join activity UI

--- app/src/main/java/com/android/capstone/yolo/layer/login/JoinActivity.java
+++ app/src/main/java/com/android/capstone/yolo/layer/login/JoinActivity.java
@@ -6,19 +6,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.capstone.yolo.R;
 import com.squareup.picasso.Picasso;
 import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageActivity;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -38,6 +37,7 @@ public class JoinActivity extends AppCompatActivity {
         pw = (EditText) findViewById(R.id.edit_join_pw);
         confirm_pw = (EditText) findViewById(R.id.edit_join_confirm_pw);
         cropImage = (CircleImageView) findViewById(R.id.image_crop);
+        cropImage.setBorderColor(Color.WHITE);
         cropImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
