9fc9a1c1533e3a047ea5e991c91ada620d01b57f
JangKyungJoo <rudwn826@gmail.com>
2017-06-06 17:18:47 +0900

    apply search

--- app/src/main/java/com/android/capstone/yolo/service/SearchService.java
+++ app/src/main/java/com/android/capstone/yolo/service/SearchService.java
@@ -1,7 +1,6 @@
 package com.android.capstone.yolo.service;
 
 import com.android.capstone.yolo.model.BoardList;
-import com.android.capstone.yolo.model.SearchResult;
 
 import java.util.List;
 
@@ -12,9 +11,6 @@ import retrofit2.http.Query;
 
 public interface SearchService {
 
-    @GET("/api/board")
-    Call<SearchResult> search(@Path("query") String query, @Query("access_token") String token);
-
-    @GET("/api/board")
-    Call<List<BoardList>> tempSearch();
+    @GET("/api/board/search/{query}")
+    Call<List<BoardList>> search(@Path("query") String query, @Query("access_token") String token);
 }
